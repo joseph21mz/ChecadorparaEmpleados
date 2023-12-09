aa04cbb91aebafe62d24668de93122973d1ef5e5
JOSE LUIS <jose.meza21@gmail.com>
2023-12-09 12:17:06 -0700

    se suben cambios para diseño del proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/services.dart';
-import 'dart:async'; // Asegúrate de importar esta librería
+// import 'package:mysql1/mysql1.dart';
+import 'dart:async';
+
+// import 'package:mysql_client/mysql_client.dart';
+
+// import 'package:mysql1/mysql1.dart'; // Asegúrate de importar esta librería
+
+
+String _host = '';
+
+Future main() async {
+
+
+  await dotenv.load();
+
+  _host = dotenv.get('HOST');
+
+  // String foo = dotenv.get('VAR_NAME');
+
+  // // Or with fallback.
+  // String bar = dotenv.get('MISSING_VAR_NAME', fallback: 'sane-default');
+
+  // // This would return null.
+  // String? baz = dotenv.maybeGet('MISSING_VAR_NAME', fallback: null);
 
 
 
-void main() {
   runApp(const MyApp());
 }
+
+
+// void main() {
+//   runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,6 +45,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Checador para Empleados',
       theme: ThemeData(
 
@@ -41,42 +70,45 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String codigoEmpleado ='';
-  TextEditingController _controller = TextEditingController(); // Declarar el controller
+  final TextEditingController _controller = TextEditingController(); // Declarar el controller
   Stream<DateTime> _clock() async* {
     while (true) {
-      await Future.delayed(Duration(seconds: 1));
+      await Future.delayed(const Duration(seconds: 1));
       yield DateTime.now();
     }
   }
 
   Future<String> registrarEntrada(codigoEmpleado) async {
-    var url = Uri.parse('http://localhost:3000/api/registrarEntrada');
+    // var url = Uri.parse('http://localhost:3000/api/registrarEntrada');
     var nombre ='';
-    print("entrada $codigoEmpleado");
-    // Datos que deseas enviar al servidor
-    var data = {
-      "iTipo": 1,
-      "iGEEMCodigo": 8,
-      "iGESUCodigo": 1,
-      "iPEEMCodigo": codigoEmpleado,
-      "iPEVATipo": 0,
-      "iMant": 0
-    };
+    // print("entrada $codigoEmpleado");
+    // // Datos que deseas enviar al servidor
+    // var data = {
+    //   "iTipo": 1,
+    //   "iGEEMCodigo": 8,
+    //   "iGESUCodigo": 1,
+    //   "iPEEMCodigo": codigoEmpleado,
+    //   "iPEVATipo": 0,
+    //   "iMant": 0
+    // };
      
     try {
       var response = await http.post(
-        url,
+        Uri.parse('http://facturacionpemex.dyndns.org/api/registrarentrada'),
         headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: jsonEncode(data),
+        body: <String, String>{
+          'PEEMCodigo': codigoEmpleado.toString(),
+        },
       );
 
       if (response.statusCode == 201) {
         print('Datos enviados correctamente');
         print(response.body); // Si necesitas ver la respuesta del servidor
         var jsonResponse = json.decode(response.body);
-        nombre = jsonResponse[0][0]['EMPLEADO_NOMBRE'];
+        nombre = jsonResponse['EMPLEADO_NOMBRE'];
+        // nombre = jsonResponse[0]['PEEMNombre'];
         print(nombre);
       } else {
         print('Error al enviar datos: ${response.statusCode}');
@@ -89,32 +121,43 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<String> registrarSalida(codigoEmpleado) async {
-    var url = Uri.parse('http://localhost:3000/api/registrarSalida');
+    // var url = Uri.parse('http://localhost:3000/api/registrarSalida');
     var nombre ='';
-    // Datos que deseas enviar al servidor
-    var data = {
-      "iTipo": 1,
-      "iGEEMCodigo": 8,
-      "iGESUCodigo": 1,
-      "iPEEMCodigo": codigoEmpleado,
-      "iPEVATipo": 0,
-      "iMant": 0
-    };
+    // // Datos que deseas enviar al servidor
+    // var data = {
+    //   "iTipo": 1,
+    //   "iGEEMCodigo": 8,
+    //   "iGESUCodigo": 1,
+    //   "iPEEMCodigo": codigoEmpleado,
+    //   "iPEVATipo": 0,
+    //   "iMant": 0
+    // };
 
     try {
+      // var response = await http.post(
+      //   url,
+      //   headers: <String, String>{
+      //     'Content-Type': 'application/json; charset=UTF-8',
+      //   },
+      //   body: jsonEncode(data),
+      // );
+
       var response = await http.post(
-        url,
+        Uri.parse('http://facturacionpemex.dyndns.org/api/registrarsalida'),
         headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
+          'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: jsonEncode(data),
+        body: <String, String>{
+          'PEEMCodigo': codigoEmpleado.toString(),
+        },
       );
 
       if (response.statusCode == 201) {
         print('Datos enviados correctamente');
         print(response.body); // Si necesitas ver la respuesta del servidor
         var jsonResponse = json.decode(response.body);
-        nombre = jsonResponse[0][0]['EMPLEADO_NOMBRE'];
+        nombre = jsonResponse['EMPLEADO_NOMBRE'];
+        // nombre = jsonResponse[0]['PEEMNombre'];
         print(nombre);
       } else {
         print('Error al enviar datos: ${response.statusCode}');
@@ -136,45 +179,63 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: Center(
-          child: Column(
+          child: 
+          Container(
+
+            padding: const EdgeInsets.all(20),
+            child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(
+
+              // Text('Nombre del host: ${dotenv.get('HOST')}'),
+              const Text(
                 'Hora actual:',
-                style: TextStyle(fontSize: 18),
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
               ),
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               StreamBuilder<DateTime>(
                 stream: _clock(),
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
-                    String formattedTime =
-                        '${snapshot.data!.hour}:${snapshot.data!.minute}:${snapshot.data!.second}';
+
+                    String hora ='${snapshot.data!.hour}';
+                    if( snapshot.data!.hour<10){
+                      hora = '0${snapshot.data!.hour}';
+                    }
+                    String minuto ='${snapshot.data!.minute}';
+                    if( snapshot.data!.minute<10){
+                      minuto = '0${snapshot.data!.minute}';
+                    }
+                    String segundo ='${snapshot.data!.second}';
+                    if( snapshot.data!.second<10){
+                      segundo = '0${snapshot.data!.second}';
+                    }
+                    String formattedTime = '$hora:$minuto:$segundo';
                     return Text(
                       formattedTime,
-                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      style: const TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
                     );
                   } else {
-                    return CircularProgressIndicator();
+                    return const CircularProgressIndicator();
                   }
                 },
               ),
-              SizedBox(height: 20),
-              TextButton(
+              const SizedBox(height: 20),
+              ElevatedButton(
                 onPressed: () async {
                   if (codigoEmpleado.isEmpty) {
                     showDialog(
                       context: context,
                       builder: (BuildContext context) {
                         return AlertDialog(
-                          title: Text('Error'),
-                          content: Text('Por favor, ingrese un número.'),
+                          title: const Text('Error'),
+                          content: const Text('Por favor, ingrese un número.'),
                           actions: <Widget>[
                             TextButton(
                               onPressed: () {
                                 Navigator.of(context).pop();
                               },
-                              child: Text('Aceptar'),
+                              child: const Text('Aceptar'),
                             ),
                           ],
                         );
@@ -185,12 +246,13 @@ class _MyHomePageState extends State<MyHomePage> {
                     print('ENTRO');
                     codigoEmpleado='';
                     if(nombre.isEmpty){
+                      // ignore: use_build_context_synchronously
                       showDialog(
                         context: context,
                         builder: (BuildContext context) {
                           return AlertDialog(
-                            title: Text('ENTRADA'),
-                            content: Text('El Número Ingresado no existe, favor de verificarlo'),
+                            title: const Text('ENTRADA'),
+                            content: const Text('El Número Ingresado no existe, favor de verificarlo'),
                             actions: <Widget>[
                               TextButton(
                                 onPressed: () {
@@ -198,7 +260,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                   // Limpiar el contenido del TextField después de aceptar el diálogo
                                   _controller.clear();
                                 },
-                                child: Text('Aceptar'),
+                                child: const Text('Aceptar'),
 
                               ),
                             ],
@@ -207,11 +269,12 @@ class _MyHomePageState extends State<MyHomePage> {
                       );
 
                     }else{
+                      // ignore: use_build_context_synchronously
                       showDialog(
                         context: context,
                         builder: (BuildContext context) {
                           return AlertDialog(
-                            title: Text('ENTRADA'),
+                            title: const Text('ENTRADA'),
                             content: Text('BIENVENIDO (A) $nombre'),
                             actions: <Widget>[
                               TextButton(
@@ -220,7 +283,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                   // Limpiar el contenido del TextField después de aceptar el diálogo
                                   _controller.clear();
                                 },
-                                child: Text('Aceptar'),
+                                child: const Text('Aceptar'),
 
                               ),
                             ],
@@ -231,79 +294,87 @@ class _MyHomePageState extends State<MyHomePage> {
 
                   }
                 },
-                child: Text('ENTRADA'),
+                child: const Text('ENTRADA', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
               ),
 
-              SizedBox(height: 20), // Separación entre botones
-              TextButton(
-                 onPressed: ()  async {
-                if (codigoEmpleado.isEmpty) {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text('Error'),
-                        content: Text('Por favor, ingrese un número.'),
-                        actions: <Widget>[
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                              _controller.clear();
-                            },
-                            child: Text('Aceptar'),
-                          ),
-                        ],
+              const SizedBox(height: 20), // Separación entre botones
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: ElevatedButton(
+                  onPressed: ()  async {
+                    if (codigoEmpleado.isEmpty) {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Error'),
+                            content: const Text('Por favor, ingrese un número.'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  _controller.clear();
+                                },
+                                child: const Text('Aceptar'),
+                              ),
+                            ],
+                          );
+                        },
                       );
-                    },
-                  );
-                } else {
-                  String nombre = await registrarSalida(codigoEmpleado);
-                  print('SALIO');
-                  codigoEmpleado='';
-                  if(nombre.isEmpty){
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text('SALIDA'),
-                          content: Text('Número Ingresado no Existe, favor de verificarlo'),
-                          actions: <Widget>[
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                                _controller.clear();
-                              },
-                              child: Text('Aceptar'),
-                            ),
-                          ],
+                    } else {
+                      String nombre = await registrarSalida(codigoEmpleado);
+                      print('SALIO');
+                      codigoEmpleado='';
+                      if(nombre.isEmpty){
+                        // ignore: use_build_context_synchronously
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text('SALIDA'),
+                              content: const Text('Número Ingresado no Existe, favor de verificarlo'),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                    _controller.clear();
+                                  },
+                                  child: const Text('Aceptar'),
+                                ),
+                              ],
+                            );
+                          },
                         );
-                      },
-                    );
 
-                  }else{
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text('SALIDA'),
-                          content: Text('QUE TE VAYA BIEN $nombre'),
-                          actions: <Widget>[
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                                _controller.clear();
-                              },
-                              child: Text('Aceptar'),
-                            ),
-                          ],
+                      }else{
+                        // ignore: use_build_context_synchronously
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text('SALIDA'),
+                              content: Text('QUE TE VAYA BIEN $nombre'),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                    _controller.clear();
+                                  },
+                                  child: const Text('Aceptar'),
+                                ),
+                              ],
+                            );
+                          },
                         );
-                      },
-                    );
-                  }
-                }
-                },
-                child: Text('SALIDA'),
+                      }
+                    }
+                  },
+                  child: const Text('  SALIDA  ', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                ),
+
               ),
+
+              const SizedBox(height: 20), // Separación entre botones
               TextField(
                 controller: _controller, // Asignar el controlador al TextField
                 keyboardType: TextInputType.number,
@@ -315,7 +386,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     codigoEmpleado = value; // Actualiza el valor cada vez que cambia el TextField
                   });
                 },
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   hintText: 'Ingrese su Código de Empleado',
                   border: OutlineInputBorder(),
                 ),
@@ -323,6 +394,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
             ],
           ),
+        
+            
+          )
+        
         ),
       );
     }
